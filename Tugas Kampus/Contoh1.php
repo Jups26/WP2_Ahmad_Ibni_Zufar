c80680e294d8e7074c14d5d81fdce956305d1bec
Ahmad Ibni Zufar <101938120+Jups26@users.noreply.github.com>
2022-04-06 19:45:20 +0700

    Add files via upload

---
+++ Tugas Kampus/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "nama saya Ahmad Ibni Zufar
+             saya tinggal di Rawamangun 
+             Olahraga Yang saya suka adalah
+             VOLLY";
+    }
+}
