2f312fa825e4d26c85f08a16f379eff3694aaee9
Jups26 <ahmadzufar78@gmail.com>
2022-04-06 19:37:10 +0700

    Tugas Kampus

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+<ul type="disc">
+    <li>Nama Depan : Ahmad Ibni </li>
+    <li>Nama Belakang : Zufar</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+    <li> Jalan Ekor Kuning 1 No.12</li>
+</ul>
+
+<h4>Tempat Lahir</h4>
+<ul type="none">
+    <li>Jakarta</li>
+</ul>
+
+<h4>Olah Raga Favorit</h4>
+<ul type="square">
+    <li>Volly</li>
+<li>Perang Sarung</li>
+</ul>
+</section>
+
